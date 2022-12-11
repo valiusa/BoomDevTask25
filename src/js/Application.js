59084d3c30836dccad20d1e003a6265468bf36fb
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-11 13:24:30 +0200

    bug fix

--- src/js/Application.js
+++ src/js/Application.js
@@ -14,22 +14,6 @@ export default class Application extends EventEmitter {
         this._beat = new Beat();
         this._beat.on(Beat.events.BIT, this._create.bind(this));
 
-        // const lyrics = [
-        //     "Ah",
-        //     "ha",
-        //     "ha",
-        //     "ha",
-        //     "stayin' alive",
-        //     "stayin' alive",
-        // ];
-        // let count = 0;
-
-        // const message = document.createElement("div");
-        // message.classList.add("message");
-        // message.innerText = "Ah";
-
-        // document.querySelector(".main").appendChild(message);
-
         this.emit(Application.events.READY);
     }
 
@@ -42,12 +26,14 @@ export default class Application extends EventEmitter {
             "stayin' alive",
             "stayin' alive",
         ];
-        let count = 0;
 
-        const message = document.createElement("div");
-        message.classList.add("message");
-        message.innerText = "Ah";
+        for (let i = 0; i < lyrics.length; i++) {
+            const message = document.createElement("div");
 
-        document.querySelector(".main").appendChild(message);
+            message.classList.add("message");
+            message.innerText = lyrics[i];
+
+            document.querySelector(".main").appendChild(message);
+        }
     }
 }
